55b65bd7a0966239994e1f6c082cfa827de3cb0e
Tweetsauceyeep <pobusauce12@gmail.com>
2025-10-11 22:34:47 +0800

    Commit

---
+++ MP_2_JS.js
@@ -0,0 +1,17 @@
+//********************
+//Last names: Sia
+//Language: Javascript
+//Paradigm(s):
+//********************
+
+
+function menuScreen(){
+  console.log("[1] Register Account Name")
+  console.log("[2] Register Account Name")
+  console.log("[3] Register Account Name")
+  console.log("[4] Register Account Name")
+  console.log("[5] Register Account Name")
+  console.log("[6] Register Account Name")
+}
+
+
